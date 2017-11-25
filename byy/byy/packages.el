506b1f50fe5fcdd1285a014264a1b54903963774
byyMac <471073043@qq.com>
2017-11-25 13:36:34 +0800

    rainbow colors

--- byy/byy/packages.el
+++ byy/byy/packages.el
@@ -30,7 +30,7 @@
 ;;; Code:
 
 (defconst byy-packages
-  '(company evil))
+  '(company evil rainbow-delimiters))
 
 (defun byy/post-init-company ()
   ;; (global-company-mode)
@@ -50,6 +50,20 @@
   ;; (define-key evil-normal-state-map (kbd "M-w") 'kill-buffer-and-window)
   (define-key evil-normal-state-map (kbd "C-s") 'save-buffer)
 
+  )
+
+;;; rainbow colors
+(defun byy/post-init-rainbow-delimiters ()
+  (custom-set-faces
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "green"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "green yellow"))))
+   '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
+   '(rainbow-delimiters-depth-4-face ((t (:foreground "orange"))))
+   '(rainbow-delimiters-depth-5-face ((t (:foreground "orange red"))))
+   '(rainbow-delimiters-depth-6-face ((t (:foreground "magenta"))))
+   '(rainbow-delimiters-depth-7-face ((t (:foreground "deep sky blue"))))
+   '(rainbow-delimiters-depth-8-face ((t (:foreground "wheat"))))
+   '(rainbow-delimiters-depth-9-face ((t (:foreground "wheat")))))
   )
 
 ;;; packages.el ends here
